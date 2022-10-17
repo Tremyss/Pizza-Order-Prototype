7ffc59f5578bcd9bced4fd52fa1803071d5537e8
Balázs Tildy <balazs.tild@gmail.com>
2022-10-17 17:30:59 +0200

    render html beta

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,16 @@
 console.log("script.js loaded")
 
-
 let getData = async() =>{
         const response = await fetch("http://localhost:8080/api")
         const myData = await response.json()
         console.log(myData)
+        for(const pizza of myData){
+                let name = document.createElement("h1")
+                name.setAttribute("id", pizza.id)
+                name.setAttribute("class","pizza-name")
+                name.innerText= pizza.name;
+                document.getElementById("pizzas").appendChild(name)
+        }
+
 }
 getData();
