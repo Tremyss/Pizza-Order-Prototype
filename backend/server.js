40e9eb60a5799d243b7de7f944aee4f52d5f2b70
HdA <gaborneubauer.hda@gmail.com>
2022-10-19 18:00:44 +0200

    ADD POST request functions and chart session

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,29 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/../frontend/`));
 
 app.get("/", (req, res)=> {
-
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`))
-
 })
 
 app.get("/api", (req, res)=> {
-
-
     const pizzasJson = fs.readFileSync(`./database.json`)
     const pizzasObj = JSON.parse(pizzasJson)
     res.json(pizzasObj)
-
 })
 
-
-
+app.post("/order", (req, res) => {
+    const customerName = req.body.name.replaceAll(" ", "_")
+    const date = JSON.stringify(new Date()).replaceAll(":", "-").replaceAll(".", "-").replaceAll('"', '')
+    const fileName = `${date}-${customerName}.json`
+    const orderData = JSON.stringify(req.body)
+    const uploadPath = __dirname + "/../backend/orders/" + fileName
+    fs.writeFile(uploadPath, orderData, (err) => {
+		if (err) {
+			console.log(err);
+			return res.status(500).send(err);
+		}
+	})
+    res.json(res.status)
+})
 
 app.listen(port, ()=> {
     console.log(`server is listening to port ${port}`);
